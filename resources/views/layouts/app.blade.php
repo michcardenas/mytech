f98add5298973ac2a0086699cdd991e3c17a0cb8
michael.cardenas <michael.cardenas@realtechltda.com>
2026-01-09 23:02:51 -0500

    d

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -791,6 +791,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     </style>
 
     @stack('styles')
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FDPVS72L91"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FDPVS72L91');
+</script>
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
